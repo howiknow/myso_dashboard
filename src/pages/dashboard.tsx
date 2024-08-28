7772b2cacf9667b7a53e14f0072a46caa7265564
unionc <151605395+howiknow@users.noreply.github.com>
2024-08-28 18:03:44 +0900

    퍼블리싱

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,6 +1,6 @@
+import CompareBox from "components/dashboard/compareBox";
 import CopyrightStatus from "components/dashboard/copyrightStatus";
 import st from "./dashboard.module.scss";
-
 const Dashboard = () => {
   return (
     <div className={st.container}>
@@ -8,7 +8,10 @@ const Dashboard = () => {
         <p className={st.title}>저작권 보호센터 현황보드</p>
         <button className={st.button}>상황실보기</button>
       </div>
-      <CopyrightStatus />
+      <div className={st.status_container}>
+        <CopyrightStatus />
+        <CompareBox />
+      </div>
     </div>
   );
 };

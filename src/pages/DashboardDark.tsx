9a21d37468408ce1a35fc1794bb8e6b44ce38e80
unionc <151605395+howiknow@users.noreply.github.com>
2024-09-23 16:35:16 +0900

    다크 모드 수정 중

--- src/pages/DashboardDark.tsx
+++ src/pages/DashboardDark.tsx
@@ -1,5 +1,6 @@
 import CompareBox from "components/dashboard/darkmode/CompareBox";
 import CopyRightAmount from "components/dashboard/darkmode/CopyRightAmount";
+import CopyRightManage from "components/dashboard/darkmode/CopyRightManage";
 import Title from "components/dashboard/darkmode/Title";
 import st from "./DashboardDark.module.scss";
 
@@ -7,9 +8,14 @@ const DashboardDark = () => {
   return (
     <div className={st.bg_container}>
       <Title />
-      <div className={st.amount_container}>
-        <CompareBox />
-        <CopyRightAmount />
+      <div className={st.box_container}>
+        <div className={st.amount_container}>
+          <CompareBox />
+          <CopyRightAmount />
+        </div>
+        <div>
+          <CopyRightManage />
+        </div>
       </div>
     </div>
   );

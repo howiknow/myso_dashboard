7373ea9026780e72865552cef6212414b624c54b
unionc <151605395+howiknow@users.noreply.github.com>
2024-09-26 18:01:30 +0900

    퍼블리싱

--- src/components/dashboard/darkmode/AllUpdate.tsx
+++ src/components/dashboard/darkmode/AllUpdate.tsx
@@ -1,15 +1,20 @@
+import ArrowUp from "assets/svg/arrow_up.svg";
 import Person from "assets/svg/person.svg";
 import st from "./AllUpdate.module.scss";
 
 const AllUpdata = () => {
   return (
     <div className={st.container}>
-      <div>
+      <div className={st.amount}>
+        <img src={Person} className={st.img}></img>
         <div>
-          <img src={Person}></img>
-          <div>
-            <p>전체회원수 현황</p>
-            <p>23,423 </p>
+          <p className={st.subtitle}>전체회원수 현황</p>
+          <div className={st.status}>
+            <p className={st.count}>23,423 </p>
+            <div className={st.percnet}>
+              <img src={ArrowUp}></img>
+              <p className={st.percentage}>65%</p>
+            </div>
           </div>
         </div>
       </div>
--- src/components/dashboard/darkmode/BoxItem.tsx
+++ src/components/dashboard/darkmode/BoxItem.tsx
@@ -1,3 +1,4 @@
+import ArrowUp from "assets/svg/arrow_up.svg";
 import st from "./BoxItem.module.scss";
 
 interface BoxItemProps {
@@ -18,7 +19,10 @@ const BoxItem = ({ imgSrc, title, description }: BoxItemProps) => {
       </div>
       <div className={st.additional_info_container}>
         <p className={st.additional_info}>전월대비</p>
-        <div className={st.info_icon}></div>
+        <div className={st.info_icon}>
+          <img src={ArrowUp} className={st.img} />
+          <p className={st.amount}>25%</p>
+        </div>
       </div>
     </div>
   );
